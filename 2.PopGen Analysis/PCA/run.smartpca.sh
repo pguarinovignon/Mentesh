93bc23dcc68df4b309202813f03ce46cdad51b1b
Perle Guarino-Vignon <43336932+pguarinovignon@users.noreply.github.com>
2023-03-01 17:04:01 +0100

    Update run.smartpca.sh

--- 2.PopGen Analysis/PCA/run.smartpca.sh
+++ 2.PopGen Analysis/PCA/run.smartpca.sh
@@ -3,7 +3,7 @@ snpname: ../../data/1240v2_HO_NP_final_MDH_laza2022.snp
 indivname: ../../data/1240v2_HO_NP_final_MDH_laza2022.ind
 evecoutname:1240v2_HO_NP_final_MDH_laza2022_eurasie_focus_caucase.evec
 evaloutname: 1240v2_HO_NP_final_MDH_laza2022_eurasie_focus_caucase.eval
-poplistname: toinclude_eurasie_focus_caucase_noEA_HO
+poplistname: metadata/eurasia_noEastAsia.txt
 lsqproject: YES
 outliermode: 2" > par_pca_eurasie.txt
 
