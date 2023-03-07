12b26150e2a4a05ec01bb4cfba52e0e022c15298
Perle Guarino-Vignon <43336932+pguarinovignon@users.noreply.github.com>
2023-03-07 15:59:17 +0100

    Update run.dstats.sh

--- 2.PopGen Analysis/Dstats/run.dstats.sh
+++ 2.PopGen Analysis/Dstats/run.dstats.sh
@@ -12,5 +12,8 @@ for i in {1..1501..50} ; do echo "qpDstat -p Dstat_MT_Caucase_Paleo_Neo_select.p
 grep result log_Dstat_Mentesh_Paleo_Neo_select_* > out_Dstat_Mentesh_Paleo_Neo_select
 grep result log_Dstat_MT_Paleo_Neo_select_* > out_Dstat_MT_Paleo_Neo_select
 
-for i in {1..301..50} ; do echo "qpDstat -p Dstat_Mentesh_caucase_BA.par -L $i -H $(($i+49)) > log_Dstat_Mentesh_caucase_BA_$i" ; done | parallel
-for i in {1..351..50} ; do echo "qpDstat -p Dstat_Mentesh_anat_BA.par -L $i -H $(($i+49)) > log_Dstat_Mentesh_anat_BA_$i" ; done | parallel
+for i in {1..151..50} ; do echo "qpDstat -p Dstat_Mentesh_caucase_BA.par -L $i -H $(($i+49)) > log_Dstat_Mentesh_caucase_BA_$i" ; done | parallel
+for i in {1..101..50} ; do echo "qpDstat -p Dstat_Mentesh_anat_BA.par -L $i -H $(($i+49)) > log_Dstat_Mentesh_anat_BA_$i" ; done | parallel
+
+grep result log_Dstat_Mentesh_caucase_BA_* > out_Dstat_Mentesh_caucase_BA
+grep result log_Dstat_Mentesh_anat_BA_* > out_Dstat_Mentesh_anat_BA
